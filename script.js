6a068339cf884a3fcbda23494c4ba9ef54b295fc
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-14 12:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// let input1 = prompt("Enter the first number:");
-// let input2 = prompt("Enter the second number:");
+let input1 = prompt("Enter the first number:");
+let input2 = prompt("Enter the second number:");
 
 // Write your code here and print the output using alert function
 
