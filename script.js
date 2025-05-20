f732e68345a7a223858373cc739ce0991a001432
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-20 21:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
 
+
 // Write your code here and print the output using alert function
 
 function addNumber(input1 , input2){
 	input1 = +input1;
 	input2 = +input2;
 
-	if(!input1 || input2 || NaN(input1) || NaN(input2)){
+	if(!input1 || !input2 || NaN(input1) || NaN(input2)){
 		return "Invalid input. Please enter a valid number.");
 		
 	}
